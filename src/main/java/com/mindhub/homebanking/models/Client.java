cd327cdaefbede3d3dca370396a9ceb93808795e
ezemach <97995869+ezemach@users.noreply.github.com>
2023-04-19 09:48:22 -0300

    Add files via upload

---
+++ src/main/java/com/mindhub/homebanking/models/Client.java
@@ -0,0 +1,123 @@
+package com.mindhub.homebanking.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static java.util.stream.Collectors.toList;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    @OneToMany(mappedBy="owner", fetch=FetchType.EAGER)
+    private Set<Account> accounts = new HashSet<>();
+
+    @OneToMany(mappedBy="owner", fetch=FetchType.EAGER)
+    private Set<ClientLoan> clientLoans = new HashSet<>();
+
+    @OneToMany(mappedBy="owner", fetch=FetchType.EAGER)
+    private Set <Card> cards = new HashSet<>();
+
+
+    public Client () {}
+
+
+    public Client(String first, String last, String mail) {
+        this.firstName = first;
+        this.lastName = last;
+        this.email = mail;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+
+    public Set<Account> getAccounts(){
+        return accounts;
+    }
+
+    public void addAccount (Account account){
+        account.setOwner(this);
+        accounts.add(account);
+    }
+
+    public void setAccounts(Set<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    public Set<Card> getCards() {
+        return cards;
+    }
+
+    public void setCards(Set<Card> cards) {
+        this.cards = cards;
+    }
+
+    public void addCard (Card card){
+        card.setOwner(this);
+        cards.add(card);
+    }
+
+
+    public void addClientLoan(ClientLoan clientLoan) {
+        clientLoan.setOwner(this);
+        clientLoans.add(clientLoan);
+    }
+
+    public Set<ClientLoan> getClientLoans() {
+        return clientLoans;
+    }
+
+    public void setClientLoans(Set<ClientLoan> clientLoans) {
+        this.clientLoans = clientLoans;
+    }
+
+    @JsonIgnore
+    public List<Loan> getLoans() {
+        return clientLoans.stream().map(sub -> sub.getLoan()).collect(toList());
+    }
+
+
+}
